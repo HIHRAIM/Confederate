305eac2f187bda287ad93f8a8081f975af617ee8
Ĥiĥraim Tanenliĥt <70520210+HIHRAIM@users.noreply.github.com>
2025-10-12 17:34:59 +0300

    Delete testzone directory

--- testzone/___init___.py
+++
@@ -1,3 +0,0 @@
-# Этот файл должен быть пустым# Create empty __init__.py files if they don't exist
-echo # > C:\Users\jmtub\Desktop\relaybot2\__init__.py
-echo # > C:\Users\jmtub\Desktop\relaybot2\relaybot\__init__.py
