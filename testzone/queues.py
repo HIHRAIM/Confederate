c822df1bb1051be65acf04f54e1fd09804aa3ad4
Ĥiĥraim Tanenliĥt <70520210+HIHRAIM@users.noreply.github.com>
2026-02-20 09:35:15 +0200

    Delete testzone directory
    Не нужно

--- testzone/queues.py
+++
@@ -1,7 +0,0 @@
-import asyncio
-
-class RelayQueues:
-    def __init__(self):
-        self.discord_to_telegram = asyncio.Queue()
-        self.telegram_to_discord = asyncio.Queue()
-        self.telegram_to_telegram = asyncio.Queue()
